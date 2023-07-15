d7719d03875f15993f4652349bc077c60c97b94b
ahmed hussien <ahmed.hussien.vax@gmail.com>
2023-07-15 02:51:09 -0700

    c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: c program that shows a sentence
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
