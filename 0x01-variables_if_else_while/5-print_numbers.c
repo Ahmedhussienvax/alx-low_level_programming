aadba528f9a159cbca6cffc3f49ad89aaada59f1
ahmed hussien <ahmed.hussien.vax@gmail.com>
2023-07-16 08:52:17 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print 1-9
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+}
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
