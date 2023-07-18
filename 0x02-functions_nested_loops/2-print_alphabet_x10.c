adab019e0e343a58af3bdc70c7d8fde275f84b55
ahmed hussien <ahmed.hussien.vax@gmail.com>
2023-07-18 15:36:14 -0700

    new project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - use _putchar to print alphabet 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+}
