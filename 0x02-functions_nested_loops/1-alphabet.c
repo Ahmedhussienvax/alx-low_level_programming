860cc748ab0a01e1c187d1cea05261bc026e60cb
ahmed hussien <ahmed.hussien.vax@gmail.com>
2023-07-18 15:30:26 -0700

    new project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - use _putchar to print alphabet
  *
- * Description: print alphabet
- *
- * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
