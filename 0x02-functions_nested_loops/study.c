982ad7cc01f50e44e23bab4869db5bba9820edda
Ahmed Hussien <ahmed.hussien.vax@gmail.com>
2023-08-25 02:48:16 +0300

    singly linked

--- 0x02-functions_nested_loops/study.c
+++ 0x02-functions_nested_loops/study.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+typedef struct list_s
 {
-	char *food;
-	printf("Enter favorite food: ");
-	scanf("%s", food);
-	printf("Favorite food: %s\n", food);
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-	return 0;
+int main(void)
+{
+	list_t *node = NULL;
+	node = malloc(sizeof(list_t));
+
+	node->len = 3;
+	node->str = malloc(sizeof(char));
+* node->len;
+node->str[0] = 'H';
+node->str[1] = 'i';
+node->str[2] = '\0';
+
+node->next = NULL;
+
+free(node);
+return 0;
 }
